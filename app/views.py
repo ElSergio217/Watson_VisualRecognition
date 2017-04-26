39632640d8941f569aed92765872ea55964a4267
Sergio <Sergioa.garcia217@gmail.com>
2017-04-26 11:13:29 -0400

    files

---
+++ app/views.py
@@ -0,0 +1,12 @@
+from watson_developer_cloud import VisualRecognitionV3 as vr
+from app import app
+from flask import render_template, request
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+	if request.method == 'POST':
+		instance = vr(api_key='API_KEY_HERE', version='2016-05-20') # API credentials
+		img = instance.classify(images_url=request.form['search']) # Identify Image  
+		return render_template('result.html', img=img) # Renders template with Image results
+	return render_template('index.html')
+
