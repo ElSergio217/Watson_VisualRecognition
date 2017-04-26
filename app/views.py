62b7f00755b34a7ad480151d1bb511a435d21644
Sergio <Sergioa.garcia217@gmail.com>
2017-04-26 12:14:17 -0400

    files

--- app/views.py
+++ app/views.py
@@ -4,18 +4,14 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-	# if request.method == 'POST':
-	# 	instance = vr(api_key='ea063a17f1589193f1f83656d6608806eee905d0', version='2016-05-20') # API credentials
-	# 	img = instance.classify(images_url=request.form['search']) # Identify Image  
-	# 	return render_template('result.html', img=img) # Renders template with Image results
+	if request.method == 'POST':
+		instance = vr(api_key='ea063a17f1589193f1f83656d6608806eee905d0', version='2016-05-20') # API credentials
+		img = instance.classify(images_url=request.form['search']) # Identify Image  
+		return render_template('result.html', img=img) # Renders template with Image results
 	return render_template('index.html')
 
 
 if __name__ == '__main__':
-	port = int(os.environ.get('PORT', 5000))
-
-	if port == 5000:
-		app.debug = True
-	app.run(host='0.0.0.0', port=port)
+	app.run()
