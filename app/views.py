6b5dd1749c64f7a49f6a865d6e5ee06e242c64c3
Sergio <Sergioa.garcia217@gmail.com>
2017-04-26 12:17:34 -0400

    files

--- app/views.py
+++ app/views.py
@@ -6,7 +6,7 @@ from flask import render_template, request
 @app.route('/', methods=['GET', 'POST'])
 def index():
 	if request.method == 'POST':
-		instance = vr(api_key='ea063a17f1589193f1f83656d6608806eee905d0', version='2016-05-20') # API credentials
+		instance = vr(api_key='API_KEY_GOES_HERE', version='2016-05-20') # API credentials
 		img = instance.classify(images_url=request.form['search']) # Identify Image  
 		return render_template('result.html', img=img) # Renders template with Image results
 	return render_template('index.html')
